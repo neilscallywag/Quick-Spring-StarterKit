4344123ee6b4db2f686145f0cacfdb3477b29871
Lucerne Loke <46553691+anonymouspanda-23@users.noreply.github.com>
2024-08-12 18:33:55 +0800

    Recommender system working now. (#3)
    * fix: recommender service and configured opensearch for local use
    
    fix: english cmi
    
    chore: configure opensearch
    
    chore: add type checker in kafka consumer.
    
    chore: removed pyinstaller and second build stage temporarily due to issues
    
    chore: removed pyinstaller and second build stage temporarily due to issues
    
    fix: fixed issue where recommender service did not work with pyinstaller
    
    fix: fixed issue with java localdatetime parsing
    
    * chore: made requested changes

--- backend/recommendations-service/app/config.py
+++ backend/recommendations-service/app/config.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic_settings import BaseSettings
 
 
@@ -7,8 +9,9 @@ class Settings(BaseSettings):
     kafka_topic: str = "job_events"
     opensearch_host: str = "http://opensearch:9200"
     opensearch_index: str = "user_recommendations"
+    opensearch_auth: List[str] = ["admin", "admin"]
     db_url: str = "postgresql+asyncpg://user:password@postgres/dbname"
-    num_clusters: int = 3  # Number of clusters for user segmentation
+    num_clusters: int = 1  # Number of clusters for user segmentation (Using 1 cluster for testing as I only have 1 sample user)
 
 
 settings = Settings()
